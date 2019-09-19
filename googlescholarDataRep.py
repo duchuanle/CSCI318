11cf6759adf84ddea83609a92eaf50ceea52beea
Patrick Jonathan Young <patrickjyoung97@gmail.com>
2019-09-19 10:18:31 +1000

    added some table code unfinished

--- googlescholarDataRep.py
+++ googlescholarDataRep.py
@@ -4,8 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import matplotlib.pyplot as plt
 import scholarly
-import xml.etree.ElementTree as ET
-tree = ET.parse(test.xml)
+
+
+
 
 
 def howManyHyphens(title):
@@ -55,8 +56,39 @@ def main():
     plt.ylabel('Mean Citation Count')
     plt.title('Mean Citation Count and Hyphens')
 
-    plt.show()
     
+
+    totalcount = 0
+    for x in articleCount:
+        totalcount += x
+    
+
+    plt.show()
+
+##    columns = ['No. of Hyphens','0','1','2','3','4','>4','Overall']
+##    rows = ['No of papers','Percentage']
+##    data = [[articleCount[0],articleCount[0]/totalcount],
+##                        [articleCount[1],articleCount[1]/totalcount],[articleCount[2],articleCount[2]/totalcount],
+##                         [articleCount[3],articleCount[3]/totalcount],[articleCount[4],articleCount[4]/totalcount],
+##                         [articleCount[5],articleCount[5]/totalcount],[totalcount,100]
+##                         ]
+##
+##    colors = plt.cm.BuPu(np.linspace(0, 0.5, len(rows)))
+##
+##    
+##    the_table = plt.table(cellText=data,
+##                      rowLabels=rows,
+##                      rowColours=colors,
+##                      colLabels=columns,
+##                      loc='bottom')
+##
+##    
+##    
+##
+##    plt.subplots_adjust(left=0.2, bottom=0.2)
+##
+##    plt.show()
+
 
 main()
 
